5f64f62acbe7c94f9fc0ec53e075fdcf99276238
zzokku <93818329+zzokku@users.noreply.github.com>
2023-03-24 15:47:42 +0200

    puhelinluettelo backend 3.1-3.6

---
+++ osa-3/puhelinluettelo-backend/index.js
@@ -0,0 +1,82 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+let persons = [
+    {
+        id: 1,
+        name: "Arto Hellas",
+        number: "040-123456"
+    },
+    {
+        id: 2,
+        name: "Ada Lovelace",
+        number: "39-44-5323523"
+    },
+    {
+        id: 3,
+        name: "Dan Abramov",
+        number: "12-43-234345",
+    },
+    {
+        id: 4,
+        name: "Mary Poppendick",
+        number: "39-23-6423122"
+    }
+]
+
+app.get('/api/persons', (request, response) => {
+    console.log(persons)
+    response.json(persons)
+})
+
+
+app.get('/info', (request, response) => {
+    response.send("Phonebook has info for "+persons.length+" people"+"<p>" + new Date().toLocaleString() + "</p>")
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    if (person){
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+    response.status(204).end()
+})
+
+
+app.post('/api/persons', (request, response) => {
+    const person = request.body
+    person.id = (Math.random() * 10000 | 0)
+    
+    if (!person.name || !person.number) {
+        return response.status(400).json({ 
+          error: 'name or number missing' 
+        })
+    }
+
+    if (persons.find(p => p.name === person.name)){
+        return response.status(422).json({ 
+            error: 'name must be unique' 
+        })
+    }
+    
+    console.log(person)
+    persons = persons.concat(person)
+    console.log(persons)
+    response.json(person)
+})
+
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
