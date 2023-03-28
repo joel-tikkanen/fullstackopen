db8bf8012b48705a49fe5f47de4ea483397cbbf3
zzokku <93818329+zzokku@users.noreply.github.com>
2023-03-28 23:44:07 +0300

    4.8-4.14

--- osa-4/blogilista/controllers/blogs.js
+++ osa-4/blogilista/controllers/blogs.js
@@ -16,6 +16,28 @@ blogRouter.post('', (request, response) => {
     .then(result => {
       response.status(201).json(result)
     })
+    .catch(() => {
+      response.status(400).end()
+    })
+})
+
+blogRouter.delete('/:id', (request, response) => {
+  Blog
+    .findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).json(result)
+    })
+    .catch(err => {
+      console.log(err)
+      response.status(400).end()
+    })
+})
+
+blogRouter.put('/:id', async (request, response) => {
+  const body = request.body
+  console.log(body)
+  await Blog.findByIdAndUpdate(request.params.id, body, {new: true})
+  response.end()
 })
 
 module.exports = blogRouter
